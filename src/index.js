64e22812433d70ddfc538153cf774cd2457d7939
Minh20812 <blossworld12@gmail.com>
2025-07-12 22:27:22 +0700

    change for github actions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
-import cron from "node-cron";
 import { runRssJob } from "./tasks/rssJob.js";
 
-const TIMEZONE = "Asia/Ho_Chi_Minh";
+runRssJob()
+  .then(() => {
+    console.log("✅ RSS job finished.");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error("❌ Error:", err.message);
+    process.exit(1);
+  });
 
-cron.schedule(
-  "0 6,11,16,21 * * *",
-  () => {
-    console.log(
-      `[CRON] Bắt đầu job theo lịch lúc ${new Date().toLocaleString("vi-VN", {
-        timeZone: TIMEZONE,
-      })}`
-    );
-    runRssJob();
-  },
-  {
-    timezone: TIMEZONE,
-  }
-);
+// import cron from "node-cron";
+// import { runRssJob } from "./tasks/rssJob.js";
 
-console.log(
-  `[INIT] Khởi động job ngay lúc ${new Date().toLocaleString("vi-VN", {
-    timeZone: TIMEZONE,
-  })}`
-);
-runRssJob();
+// const TIMEZONE = "Asia/Ho_Chi_Minh";
+
+// cron.schedule(
+//   "0 6,11,16,21 * * *",
+//   () => {
+//     console.log(
+//       `[CRON] Bắt đầu job theo lịch lúc ${new Date().toLocaleString("vi-VN", {
+//         timeZone: TIMEZONE,
+//       })}`
+//     );
+//     runRssJob();
+//   },
+//   {
+//     timezone: TIMEZONE,
+//   }
+// );
+
+// console.log(
+//   `[INIT] Khởi động job ngay lúc ${new Date().toLocaleString("vi-VN", {
+//     timeZone: TIMEZONE,
+//   })}`
+// );
+// runRssJob();
